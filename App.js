1b18a48e8c19c069d5c5da2beaf137d437f79619
joshipiyush9969 <piyushjb7@hotmail.com>
2021-09-03 00:55:18 +0530

    fb login on progress

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View,Alert } from 'react-native';
+import * as Facebook from "expo-facebook";
 
 export default function App() {
+
+  async function logIn() {
+    try {
+      await Facebook.initializeAsync({
+        appId: "388365359472152",
+      });
+      const { type, token, expirationDate, permissions, declinedPermissions } =
+        await Facebook.logInWithReadPermissionsAsync({
+          permissions: ["public_profile"],
+        });
+      if (type === "success") {
+        // Get the user's name using Facebook's Graph API
+        const response = await fetch(
+          `https://graph.facebook.com/me?access_token=${token}`
+        );
+        Alert.alert("Logged in!", `Hi ${(await response.json()).name}!`);
+      } else {
+        // type === 'cancel'
+      }
+    } catch ({ message }) {
+      alert(`Facebook Login Error: ${message}`);
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button color='blue' title='Facebook Login' onPress={logIn}/>
       <StatusBar style="auto" />
     </View>
   );
