98f41a578ea1556f18fb21a696b23f6b26993d0d
joshipiyush9969 <piyushjb7@hotmail.com>
2021-09-03 13:29:50 +0530

    facebook linked, log out is remaining

--- App.js
+++ App.js
@@ -5,20 +5,31 @@ import * as Facebook from "expo-facebook";
 
 export default function App() {
 
+
   async function logIn() {
     try {
       await Facebook.initializeAsync({
-        appId: "388365359472152",
+        appId: "436088394405707",
       });
       const { type, token, expirationDate, permissions, declinedPermissions } =
         await Facebook.logInWithReadPermissionsAsync({
           permissions: ["public_profile"],
         });
-      if (type === "success") {
-        // Get the user's name using Facebook's Graph API
-        const response = await fetch(
-          `https://graph.facebook.com/me?access_token=${token}`
+      console.log('&&&&',token)
+       if (type === "success") {
+         // Get the user's name using Facebook's Graph API
+         const response = await fetch(
+           `https://graph.facebook.com/me?access_token=${token}}`
+         );
+         const resData = await response.json()
+        console.log(resData)
+        console.log(resData.id)
+
+        const profileData = await fetch(
+          `https://graph.facebook.com/${resData.id}?fields=picture&access_token=${token}`
         );
+        const resProfileData = await profileData.json()
+        console.log(resProfileData)
         Alert.alert("Logged in!", `Hi ${(await response.json()).name}!`);
       } else {
         // type === 'cancel'
